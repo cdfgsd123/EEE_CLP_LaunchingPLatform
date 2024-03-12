b6e715e29a9a4a7ff4f58185d685c5063b88dd57
cdfgsd <79629165+cdfgsd123@users.noreply.github.com>
2024-03-12 10:57:14 +0800

    Update communicate.py

--- communicate.py
+++ communicate.py
@@ -2,5 +2,17 @@
 
 import serial
 
-ser = serial.Serial(5,115200)
+com = "COM5"
+baudrate = 115200
+serialString = ""
+
+ser = serial.Serial(com,baudrate)
 ser.open()
+
+while(1):
+  serialString = ser.readline()
+
+  try:
+    print(serialString.decode("HEX"))
+  except:
+    pass
