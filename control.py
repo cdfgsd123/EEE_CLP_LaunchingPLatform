089487c45b7540b4c3bc758a47207b00af118c2c
superjawk <103614651+superjawk@users.noreply.github.com>
2024-05-14 21:55:04 +0800

    add slot number function  and reduce input args

--- control.py
+++ control.py
@@ -1,6 +1,5 @@
 # communicate
 import threading
-import time
 
 import serial
 
@@ -8,75 +7,107 @@ import serial
 class ServoControl:
     def __init__(self, targetPos):
         self.receivedString = ""
-        self.pos = 0
-        self.targetPos = targetPos
-        self.command = None
-
+        self.pos = 0  # current position in degree
+        self.targetPos = targetPos  # desired position
+        self.command = None  # control command
+        self.directions = ["a", "d", "s", "w"]
+        # acceptable angle error
+        self.lowThreshold = 5  # acceptable angle error
+        self.highThreshold = 20  # for calcuating speed of rotation
+        self.thresholdRange = self.highThreshold - self.lowThreshold
+        self.angleDivision = 360 // 30
+        # speed of servo (self-defined)
         self.maxSpeed = 0xCC
         self.minSpeed = 0x90
+        self.speedRange = self.maxSpeed - self.minSpeed
 
+        # 6 byte motion control signal to ESP32
         self.RightString = [0x60, 0x0B, 0xFF, 0xFF, 0xFF, 0xFF]
         self.LeftString = [0x60, 0x0B, 0x00, 0x00, 0x00, 0x00]
 
-        self.RightStringMax = [0x60, 0x0B, 0xFF, 0xFF, 0xFF, 0xFF]
-        self.LeftStringMax = [0x60, 0x0B, 0x00, 0x00, 0x00, 0x00]
+        self.backwardString = [0x60, 0x0B, 0x90, 0x90, 0x70, 0x70]
+        self.forwardString = [0x60, 0x0B, 0x70, 0x70, 0x90, 0x90]
+
         self.stopString = [0x60, 0x0B, 0x80, 0x80, 0x80, 0x80]
 
-        self.ser = serial.Serial(port="COM6", baudrate=115200, timeout=0.01)
-        self.position = 0
-        self.directions = ["a", "d", "s", "w"]
+        # serial connection
+        self.ser = serial.Serial(port="COM5", baudrate=115200, timeout=0.1)
+
+    #  setter and getter
+    def setCommand(self, input):
+        self.command = input
 
     def setTargetPos(self, input):
-        self.targetPos = input
+        self.targetPos = int(input)
+        self.updateTargetPos()
 
+    def getTargetPos(self):
+        return self.targetPos
+
+    def getPos(self):
+        return self.pos
+
+    def getSlotNum(self):
+        slot = self.pos // self.angleDivision
+        return int(slot)
+
+    def setTargetPosBySlotNum(self, slotNum):
+        self.targetPos = slotNum * self.angleDivision + self.angleDivision // 2
+
+    # 4 directions motion and stop
     def turnLeft(self, speed):
-        # assume move servo 1 and 3
         self.LeftString[2] = self.maxSpeed - speed  # servo 1
         self.LeftString[4] = self.maxSpeed - speed  # servo 3
+
+        self.LeftString[3] = self.maxSpeed - speed  # servo 2
+        self.LeftString[5] = self.maxSpeed - speed  # servo 4
         self.ser.write(serial.to_bytes(self.LeftString))
 
     def turnRight(self, speed):
-        # assume move servo 1 and 3
         self.RightString[2] = speed  # servo 1
         self.RightString[4] = speed  # servo 3
+
+        self.RightString[3] = speed  # servo 2
+        self.RightString[5] = speed  # servo 4
         self.ser.write(serial.to_bytes(self.RightString))
 
     def stop(self):
         self.ser.write(serial.to_bytes(self.stopString))
 
-    def showOutput(self, targetPos, command=None):
+    def goForward(self):
+        self.ser.write(serial.to_bytes(self.forwardString))
 
+    def goBack(self):
+        self.ser.write(serial.to_bytes(self.backwardString))
+
+    # show output and update logic
+    def showOutput(self, command=None):
+        self.command = command
         receivedString = self.ser.readline()
         if len(receivedString) >= 25:
             self.pos = int(int(receivedString[25]) / 255 * 360)  # current pos
-            # print(receivedString)
+            print(receivedString)
 
-            # print("Degree = ", pos)
-            self.updatePos(command, targetPos)
-            process = True
+            print("Degree = ", self.pos)
+            print("targetPos = ", self.targetPos)
 
-            self.pos, process = self.dstop(command, self.pos, self.targetPos)
+            self.updateTargetPos()
 
-            # print()
-        return int(targetPos)
+            stopped = self.dstop(command, self.pos, self.targetPos)
+            print("stopped: ", stopped)
+            print()
+        return int(self.targetPos)
 
-    def dstop(self, command, pos, targetPos):
+    # determine move or stop
+    def dstop(self):
         # print("pos:", pos, " targetPos: ", targetPos)
-        if abs(targetPos - pos) <= 5:
+        if abs(self.targetPos - self.pos) <= self.lowThreshold:
             self.stop()
-            return pos, False
-        # asssume max_dispalcement 20
-        distance = abs(targetPos - pos)
-        # print("displacement", displacement)
-        if distance >= 20:
-            distance = 20
-        range
-        speed = int(
-            (self.maxSpeed - self.minSpeed) * (distance / 20) + self.minSpeed,
-        )
-        # print("speed", speed)
+            return True
 
-        temp = targetPos - pos
+        speed = self.getSpeed()
+
+        temp = self.targetPos - self.pos
         if temp > 0 and abs(temp) < 180:
             self.turnLeft(speed)
         elif temp > 0 and abs(temp) >= 180:
@@ -85,43 +116,72 @@ class ServoControl:
             self.turnRight(speed)
         else:
             self.turnLeft(speed)
-        return pos, True
+        return False
 
-    def updatePos(self, command, targetPos):
+    # update target position
+    def updateTargetPos(self):
         # + or minus depend on the putting angle
-        if command == "d":
-            targetPos -= 12  # right turn 12 degree
-        elif command == "a":
-            targetPos += 12  # left turn 12 degree
+        if self.command == "d":
+            self.targetPos -= 12  # right turn 12 degree
+        elif self.command == "a":
+            self.targetPos += 12  # left turn 12 degree
 
-        if targetPos >= 360:
-            targetPos -= 360
-        elif targetPos < 0:
-            targetPos += 360
-        self.setTargetPos(int(targetPos))
+        if self.targetPos >= 360:
+            self.targetPos -= 360
+        elif self.targetPos < 0:
+            self.targetPos += 360
 
+    # return a value between 0 to 1 to represent servo speed(1: max speed)
+    def getSpeedDisplay(self):
+        distance = abs(self.targetPos - self.pos)
+
+        if distance >= self.highThreshold:
+            distance = self.highThreshold
+
+        if distance < self.highThreshold:
+            return 0
+        else:
+            fraction = (distance - self.lowThreshold) / self.thresholdRange
+            return fraction
+
+    # Servo speed for motion
+    def getSpeed(self):
+        fraction = self.getSpeedDisplay()
+        if fraction == 0:
+            return 0
+        else:
+            speed = int(self.speedRange * fraction + self.minSpeed)
+            return speed
+
+    # main function
     def main(self):
-        while self.start:
+        while True:
+            print(self.command)
             if self.command is None:
-                self.showOutput(self.targetPos)
+                self.showOutput()
+            elif self.command == "x":
+                return
             else:
-                self.showOutput(self.targetPos, self.command)
+                self.showOutput(self.command)
                 self.command = None
 
 
 if __name__ == "__main__":
-    servo = ServoControl(None)
+    servo = ServoControl(0)
     job = threading.Thread(target=servo.main)
     job.start()
+
     while True:
         user_input = input("Please enter desired degree: ")
-        if user_input == "exit":
+        if user_input == "x":
             servo.stop()
+            servo.command = "x"
             break
+
         if user_input in servo.directions:  # key: a, s, d, w
             servo.command = user_input
         else:
             try:
-                servo.setTargetPos(int(user_input))
+                servo.setTargetPos(int(user_input) // 360)
             except ValueError:
                 print("Enter integer")
